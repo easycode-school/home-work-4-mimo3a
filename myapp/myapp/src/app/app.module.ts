47d947968e9c4c6e67b0b38a6795aaae1b77e652
unknown <mimo3a@gmail.com>
2019-02-11 03:12:06 +0200

    home_work_5

--- myapp/myapp/src/app/app.module.ts
+++ myapp/myapp/src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InputOutputComponentsComponent } from './components/input-output-components/input-output-components.component';
 import { FormsModule } from '@angular/forms';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputOutputComponentsComponent,
     TodoFormComponent
   ],
   imports: [
